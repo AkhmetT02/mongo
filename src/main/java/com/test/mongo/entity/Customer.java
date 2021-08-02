754c8ca001fc97db322d0cbbacd6af9bdb94adc1
Akhmet Tynymbay <axmet.messi@gmail.com>
2021-08-02 19:34:08 +0600

    Email replaced to phone

--- src/main/java/com/test/mongo/entity/Customer.java
+++ src/main/java/com/test/mongo/entity/Customer.java
@@ -9,22 +9,22 @@ public class Customer {
 
     private String firstName;
     private String lastName;
-    private String email;
+    private String phone;
 
     public Customer() {}
 
-    public Customer(String firstName, String lastName, String email) {
+    public Customer(String firstName, String lastName, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
+        this.phone = phone;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getId() {
--- src/main/java/com/test/mongo/repository/CustomerRepository.java
+++ src/main/java/com/test/mongo/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
-    List<Customer> findByEmail(String email);
+    List<Customer> findByPhone(String phone);
 }
